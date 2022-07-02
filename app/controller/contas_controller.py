bd5e47df6dc217d78b881e01758cebc0b483c975
Gabriel Dragon <gabrielsoares221@gmail.com>
2022-07-02 00:05:41 -0300

    feat: Preparando ajax para salvar conta

--- app/controller/contas_controller.py
+++ app/controller/contas_controller.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from app import app
 
 @app.route("/contas")
 def lista_contas():
   return render_template('lista_contas.html', title="Lista de Contas")
+
+@app.route("/contas/adicionar")
+def adicionar_conta():
+  pass #Adicionar no banco
